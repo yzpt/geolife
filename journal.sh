54a9002dadb76ba9b4b4164254e2a548b46ebb8d
Yohann Zapart <yzpt.de@gmail.com>
2024-10-01 14:16:30 +0200

    nb 1.0: first data extraction, modeling dataclasses

---
+++ journal.sh
@@ -0,0 +1,27 @@
+# venv
+python3 -m venv venv_geolife
+source venv_geolife/bin/activate
+
+# pip
+pip install pandas ipykernel numpy matplotlib seaborn plotly dash geopy geopandas python-dotenv st-dbscan
+pip install nbformat
+
+# gitignore
+touch .gitignore
+echo ".env" >> .gitignore
+echo "venv_*" >> .gitignore
+echo "key*" >> .gitignore
+echo "*.zip" >> .gitignore
+echo "z_*" >> .gitignore
+echo "data/*" >> .gitignore
+echo "output/*" >> .gitignore
+
+# .env
+touch .env && code .env
+
+# git
+git init
+git branch -m main
+git add .
+git commit -m "1st commit"
+
